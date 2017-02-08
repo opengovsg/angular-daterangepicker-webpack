b39d663216d0f8dbc4efdbf1e91c83294b34ea2c
David Zhang <david.zhang@wishabi.com>
2017-02-08 12:50:23 -0500

    another attempt

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ if (typeof module !== "undefined" && module.exports) {
   module.exports.daterangepicker = daterangepicker;
 } else if ((typeof define !== "undefined" && define !== null) && (define.amd !== null)) {
   define("daterangepicker", function() {
-  return daterangepicker;
-});
+    return daterangepicker;
+  })
+};
